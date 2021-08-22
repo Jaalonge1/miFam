ef3953a9066512b8a5ff632c22e5b27644d9bd18
jaalonge1 <josh.alonge@digital-clarity.io>
2021-08-22 14:20:10 -0400

    Initial Commit

---
+++ mi fam/mi_famApp.swift
@@ -0,0 +1,17 @@
+//
+//  mi_famApp.swift
+//  mi fam
+//
+//  Created by Josh Alonge on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct mi_famApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
