8eaca1460dabda98f9ae668fc2aaf25024e32ebb
jaalonge1 <josh.alonge@digital-clarity.io>
2021-08-22 23:44:47 -0400

    Initial commit, added custom font (need to set global custom font for ease-of-use

--- mi fam/ContentView.swift
+++ mi fam/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+extension Color {
+    static let pGreen = Color(red: 127 / 255, green: 212 / 255, blue: 141 / 255)
+    static let pRed = Color(red: 212 / 255, green: 135 / 255, blue: 127 / 255)
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text("welcome to ")
+                .font(Font.custom("SpaceGrotesk-Bold", size: 35)) +
+            Text("mi ")
+                .font(Font.custom("SpaceGrotesk-Bold", size: 35)) +
+            Text("fam")
+                .foregroundColor(.pGreen)
+                .font(Font.custom("SpaceGrotesk-Bold", size: 35))
+            Text("an app to stay in touch with family")
+                .font(Font.custom("SpaceGrotesk-Regular", size: 15))
+        }
+        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
