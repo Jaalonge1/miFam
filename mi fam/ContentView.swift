8f79e6ea27603148815f27748bc41bbbf2f8e496
jaalonge1 <josh.alonge@digital-clarity.io>
2021-09-05 00:50:37 -0400

    Added separating-line-image and touches to landing page

--- mi fam/ContentView.swift
+++ mi fam/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            
+            Image("famPic")
+                .resizable()
+                .frame(width: 150, height: 110, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("welcome to ")
                 .font(Font.custom("SpaceGrotesk-Bold", size: 35)) +
             Text("mi ")
@@ -25,11 +29,21 @@ struct ContentView: View {
                 .font(Font.custom("SpaceGrotesk-Bold", size: 35))
             Text("an app to stay in touch with family")
                 .font(Font.custom("SpaceGrotesk-Regular", size: 15))
+            Image("separator")
+                .resizable()
+                .frame(width: 250, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Image("landingTexts")
+                .resizable()
+                .frame(width: 380, height: 190, alignment: .center)
+            Image("separator")
+                .resizable()
+                .frame(width: 250, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
         
     }
     
 }
+
 
 
 struct ContentView_Previews: PreviewProvider {
